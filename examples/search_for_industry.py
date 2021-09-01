22dd9a7baa39f662501beb394fb6b0838b53b8d2
Darren Smith <darren.smith@sparkpost.com>
2021-09-01 11:50:11 -0400

    Update search_for_industry.py

--- examples/search_for_industry.py
+++ examples/search_for_industry.py
@@ -6,10 +6,10 @@ industry_name = "example_industry"
 time_period = "daysBack:3"
 
 ct = CompetitiveTracker(api_key)  # Instantiate Competitive Tracker
-industryId = ct.core.discover.search_industries(q=industry_name)["industries"][0]["id"]  # Use core.discover to search for the brand and pull the brandId of the first result
+industryId = ct.core.discover.search_industries(q=industry_name)["industries"][0]["id"]  # Use core.discover to search for the brand and pull the industryId of the first result
 time.sleep(5)  # Sleep for 5 seconds due to default rate limiting on the API
 results = ct.search.search(
     qd=time_period,
     industryId=industryId
-)  # Use the search function to query for all campaigns sent by the brand during the specified time window
+)  # Use the search function to query for all campaigns sent by an industry during the specified time window
 print(results)
