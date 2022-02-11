97256ffe35b83ce8a2c94aa4b4854c2e6c438c9b
Darren <darren.smith@sparkpost.com>
2022-02-11 14:28:48 -0500

    Fixed bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Darren Smith'
 
 # The full version, including alpha/beta/rc tags
 version = '1.0'
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
